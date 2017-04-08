09d64be33513797d653dc4b3b717898f3e3fd453
Pablo Repetto <caussabon@gmail.com>
2017-04-08 18:30:37 -0300

    awk -> JavaFX

--- src/Gui_first.java
+++ src/Gui_first.java
@@ -1,17 +1,35 @@
-import java.awt.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
-class Gui_first extends Frame {
+public class Gui_first extends Application {
 
-    Gui_first() {
-        Button b = new Button("click me");
-        b.setBounds(30,100,80,30);
-        add(b);
-        setSize(300,300);
-        setLayout(null);
-        setVisible(true);
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    public static void main(String args[]) {
-        Gui_first f = new Gui_first();
+    public static void main(String[] args) {
+        launch(args);
     }
 }

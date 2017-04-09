046205f07271192c9e4b6f24a1314c6e6a0d992c
Pablo Repetto <caussabon@gmail.com>
2017-04-09 04:30:46 -0300

    WiFiSocial es un Singleton

--- src/WiFiSocial.java
+++ src/WiFiSocial.java
@@ -1,7 +1,21 @@
+import java.util.Collection;
+import java.util.HashSet;
 
 public class WiFiSocial {
-    Rockola rockola = Rockola.getInstance();
-    String user;
+    static WiFiSocial singleton;
+
+    Rockola rockola;
+    Collection<String> usernames;
+
+    public static WiFiSocial getInstance() {
+        if (singleton == null) singleton = new WiFiSocial();
+        return singleton;
+    }
+
+    protected WiFiSocial() {
+        rockola = Rockola.getInstance();
+        usernames = new HashSet();
+    }
 
     public void login(String n){
         rockola.notifyLogin(n);

8a11d3762bc16bf41a27bc087af03c41efd7efbf
Pablo Repetto <caussabon@gmail.com>
2017-04-09 01:03:13 -0300

    added songs database

--- src/Rockola.java
+++ src/Rockola.java
@@ -2,10 +2,12 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Rockola {
+    Map<String, Music> songs;
     Map<String, User> users;
     Map<String, User> logged_users;
 
     public Rockola() {
+        songs = new HashMap();
         users = new HashMap();
         logged_users = new HashMap();
     }

351c6bf6fbca4c78c9dd8d2327b5fbe890d4ed25
Pablo Repetto <caussabon@gmail.com>
2017-04-08 23:18:23 -0300

    db.getUser

--- src/Db.java
+++ src/Db.java
@@ -1,12 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
 
 public class Db {
-	
-	Usuario user;
-	
-	public Usuario getUser(String n) {
-		return user;
-	}
+    Map<String, Usuario> user_store;
 
-	
+    public Db() {
+        user_store = new HashMap<String, Usuario>();
+    }
 
+    public Usuario getUser(String name) {
+        return user_store.get(name);
+    }
 }
